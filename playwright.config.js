f5bbe7ed11afb3e2dbc85550b9f48587266300bf
AntonKashichkin <Anton.Kashichkin@onlyoffice.com>
2025-10-31 19:10:30 +0300

    fix new workflows 17

--- playwright.config.js
+++ playwright.config.js
@@ -26,7 +26,7 @@ export default defineConfig({
   reporter: [
     ['line'],
     ['html', { open: 'never' }],
-    ['allure-playwright'],
+    ['allure-playwright', { outputFolder: 'allure-results' }],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
